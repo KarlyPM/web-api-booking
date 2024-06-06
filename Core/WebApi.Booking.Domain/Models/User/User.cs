be93e3c8dda78adda1e100cca7a57067ff1922b5
KarlyPM <karlitamile@hotmail.com>
2024-06-06 09:14:52 -0500

    re upload proyecto

---
+++ Core/WebApi.Booking.Domain/Models/User/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi.Booking.Domain.Models.User
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+    }
+}
