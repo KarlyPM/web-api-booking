f8660f1c3b7a87ef4e708a1de29bf58fdce495e4
KarlyPM <karlitamile@hotmail.com>
2024-06-07 10:57:21 -0500

    Agregar appsettings.json a .gitignore

--- Api/WebApi.Booking.Api/Program.cs
+++ Api/WebApi.Booking.Api/Program.cs
@@ -1,9 +1,10 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage;
 using WebApi.Booking.Application.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<IDataServicesContext>();
+//builder.Services.AddDbContext<IDataServicesContext>(options => options.UseSqlServer());
 
 var app = builder.Build();
 
